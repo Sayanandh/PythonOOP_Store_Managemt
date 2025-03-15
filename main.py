8d42a1a621ab6b9fa60e45bbbf0c82cdc91b4b27
Sayanandh Aneesh <114741767+Sayanandh@users.noreply.github.com>
2025-03-15 15:58:12 +0530

    1.1

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 class Item:
-    def __init__(self , name , price , quantity):
+    pay_rate=0.8
+    def __init__(self , name:str , price:float , quantity:int):
+        assert price>=0 , f"Price no grater than 0"
+        assert quantity>=0
         self.name = name
         self.price = price
         self.quantity = quantity
+        ls=[]
+        ls.append(self.name)
+        print(ls)
+
 
     def calu_totalprice(self):
         return self.price * self.quantity
+    
+    def applydis(self):
+        self.price=self.price * Item.pay_rate
+        print(self.price)
 
 
-item1=Item("Phone" , 1000 , 5)
-item2=Item("Laptop" , 10000 , 5)
 
-print(item1.calu_totalprice())
-
-
+item1 = Item("Phone", 100, 1)
+item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
